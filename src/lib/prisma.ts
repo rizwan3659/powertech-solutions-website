bf49ba42a5ca4449bbcf364c1ff234b7218f01df
Rizwan Alam <riscogian@gmail.com>
2026-06-07 22:19:04 +0530

    Fix Prisma Edge connection by using adapter-pg

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+  (() => {
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    return new PrismaClient({ adapter, log: ["query"] });
+  })();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
